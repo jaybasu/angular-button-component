b3452fe63b68db2fc7f13cdc0ac2d802783648d3
jaybasu <basu.jayanta@gmail.com>
2023-03-16 14:16:54 -0400

    4 files modified

--- src/app/shared/toggle-switch/toggle-switch.component.ts
+++ src/app/shared/toggle-switch/toggle-switch.component.ts
@@ -1,8 +1,10 @@
 import {
   Component,
   ElementRef,
+  EventEmitter,
   Input,
   OnInit,
+  Output,
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
@@ -13,9 +15,26 @@ import {
   styleUrls: ['./toggle-switch.component.scss'],
 })
 export class ToggleSwitchComponent implements OnInit {
-  // @Input() public imgUrl: string;
-  // @Input() public altText: string;
-  // @Input() public fontClassName: string;
+  @Input() public id: string = '';
+  @Input() public value: string = '';
+  @Input() public required: boolean = false;
+  @Input() public checked: boolean = false;
+  @Input() public disabled: boolean = false;
+  @Input() public isShowLabel: boolean = true;
+  @Input() public label: string = '';
+  @Input() public labelPosition: 'left' | 'right' = 'left';
+
+  /**
+   * Emits click event.
+   */
+  @Output() click = new EventEmitter<void>();
+  /**
+   * Emits event notifying other classes when a change in state occurs on a checkbox after a
+   * click.
+   *
+   * @deprecated since v4 use `checked` and `checkedChange` instead
+   */
+  @Output() change = new EventEmitter<any>();
 
   // @ViewChild('loadSvgImage', {
   //   static: false,
@@ -26,5 +45,26 @@ export class ToggleSwitchComponent implements OnInit {
 
   public ngOnInit(): void {
     // this.altText = this.altText ? this.altText : '';
+  }
+
+  /**
+   * Toggle the selected state of the checkbox.
+   */
+  public toggle() {
+    this.checked = !this.checked;
+    console.log(this.checked);
+  }
+  /**
+   * Handles click events on the `Checkbox` and emits changes to other classes.
+   */
+  onClick(event: Event) {
+    // Disable default checkbox activation behavior which flips checked and resets indeterminate.
+    // This allows the parent component to control the checked/indeterminate properties.
+    // event.preventDefault();
+    if (!this.disabled) {
+      this.click.emit();
+      this.toggle();
+      return;
+    }
   }
 }
