f90644d974a5d099e9a67e48b90108f23bc9c10f
jaybasu <basu.jayanta@gmail.com>
2024-07-16 22:23:30 -0400

    1 file modified, 1 added

--- src/app/shared/sharedComponent.module.ts
+++ src/app/shared/sharedComponent.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { ImgGeneratorComponent } from './img-generator/img-generator.component';
 import { ToggleSwitchComponent } from './toggle-switch/toggle-switch.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent, ImgGeneratorComponent, ToggleSwitchComponent],
-  exports: [ButtonComponent, ImgGeneratorComponent, ToggleSwitchComponent],
+  declarations: [
+    ButtonComponent,
+    ImgGeneratorComponent,
+    ToggleSwitchComponent,
+    CarouselComponent,
+  ],
+  exports: [
+    ButtonComponent,
+    ImgGeneratorComponent,
+    ToggleSwitchComponent,
+    CarouselComponent,
+  ],
 })
 export class SharedCompModule {}
