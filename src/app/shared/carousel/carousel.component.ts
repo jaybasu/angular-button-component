ab7a8bbed996b96384177a7136ecac9c19c8aa73
jaybasu <basu.jayanta@gmail.com>
2024-07-18 14:54:58 -0400

    1 file modified

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -20,52 +20,44 @@ import {
   animations: [
     trigger('slideAnimation', [
       /* scale */
-      transition('void => scale', [
-        useAnimation(scaleIn, {
-          params: { time: '${this.transitionProperties.transitionDuration}ms' },
-        }),
-      ]),
-      transition('scale => void', [
-        useAnimation(scaleOut, {
-          params: { time: '${this.transitionProperties.transitionDuration}ms' },
-        }),
-      ]),
+      transition('void => scale', this.createTransitionAnimation(scaleIn)),
+      transition('scale => void', this.createTransitionAnimation(scaleOut)),
 
-      /* fade */
-      transition('void => fade', [
-        useAnimation(fadeIn, {
-          params: { time: '${this.transitionProperties.transitionDuration}ms' },
-        }),
-      ]),
-      transition('fade => void', [
-        useAnimation(fadeOut, {
-          params: { time: '${this.transitionProperties.transitionDuration}ms' },
-        }),
-      ]),
+      // /* fade */
+      // transition('void => fade', [
+      //   useAnimation(fadeIn, {
+      //     params: { time: '${this.transitionProperties.transitionDuration}ms' },
+      //   }),
+      // ]),
+      // transition('fade => void', [
+      //   useAnimation(fadeOut, {
+      //     params: { time: '${this.transitionProperties.transitionDuration}ms' },
+      //   }),
+      // ]),
 
-      /* flip */
-      transition('void => flip', [
-        useAnimation(flipIn, {
-          params: { time: '${this.transitionProperties.transitionDuration}ms' },
-        }),
-      ]),
-      transition('flip => void', [
-        useAnimation(flipOut, {
-          params: { time: '${this.transitionProperties.transitionDuration}ms' },
-        }),
-      ]),
+      // /* flip */
+      // transition('void => flip', [
+      //   useAnimation(flipIn, {
+      //     params: { time: '${this.transitionProperties.transitionDuration}ms' },
+      //   }),
+      // ]),
+      // transition('flip => void', [
+      //   useAnimation(flipOut, {
+      //     params: { time: '${this.transitionProperties.transitionDuration}ms' },
+      //   }),
+      // ]),
 
-      /* JackInTheBox */
-      transition('void => jackInTheBox', [
-        useAnimation(jackIn, {
-          params: { time: '${this.transitionProperties.transitionDuration}ms' },
-        }),
-      ]),
-      transition('jackInTheBox => void', [
-        useAnimation(jackOut, {
-          params: { time: '${this.transitionProperties.transitionDuration}ms' },
-        }),
-      ]),
+      // /* JackInTheBox */
+      // transition('void => jackInTheBox', [
+      //   useAnimation(jackIn, {
+      //     params: { time: '${this.transitionProperties.transitionDuration}ms' },
+      //   }),
+      // ]),
+      // transition('jackInTheBox => void', [
+      //   useAnimation(jackOut, {
+      //     params: { time: '${this.transitionProperties.transitionDuration}ms' },
+      //   }),
+      // ]),
     ]),
   ],
 })
@@ -97,6 +89,13 @@ export class CarouselComponent implements OnInit {
     this.currentSlideIndex =
       (this.currentSlideIndex - 1 + this.slides.length) % this.slides.length;
   }
-
+  createTransitionAnimation(animationFn: AnimationReferenceMetadata) {
+    console.log(this.transitionProperties.transitionDuration);
+    return [
+      useAnimation(animationFn, {
+        params: { time: `${this.transitionProperties.transitionDuration}ms` },
+      }),
+    ];
+  }
   // public slideTransitionStyle = this.transitionProperties.transitionStyle;
 }
