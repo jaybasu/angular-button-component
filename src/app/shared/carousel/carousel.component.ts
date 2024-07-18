fb15b673c1fbc752496d75027d2a4b1bd410fe40
jaybasu <basu.jayanta@gmail.com>
2024-07-17 22:33:07 -0400

    2 files modified

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -19,12 +19,36 @@ import {
   styleUrls: ['./carousel.component.scss'],
   animations: [
     trigger('slideAnimation', [
+      /* scale */
+      transition('void => scale', [
+        useAnimation(scaleIn, { params: { time: '500ms' } }),
+      ]),
+      transition('scale => void', [
+        useAnimation(scaleOut, { params: { time: '500ms' } }),
+      ]),
+
       /* fade */
       transition('void => fade', [
         useAnimation(fadeIn, { params: { time: '500ms' } }),
       ]),
       transition('fade => void', [
         useAnimation(fadeOut, { params: { time: '500ms' } }),
+      ]),
+
+      /* flip */
+      transition('void => flip', [
+        useAnimation(flipIn, { params: { time: '500ms' } }),
+      ]),
+      transition('flip => void', [
+        useAnimation(flipOut, { params: { time: '500ms' } }),
+      ]),
+
+      /* JackInTheBox */
+      transition('void => jackInTheBox', [
+        useAnimation(jackIn, { params: { time: '700ms' } }),
+      ]),
+      transition('jackInTheBox => void', [
+        useAnimation(jackOut, { params: { time: '700ms' } }),
       ]),
     ]),
   ],
@@ -33,7 +57,7 @@ export class CarouselComponent implements OnInit {
   @Input() slides: Slide[] = [];
   @Input() transitionProperties: TransitionProperties = {
     // Default values
-    transitionStyle: 'fade',
+    transitionStyle: 'scale',
     slideDirection: 'left',
     transitionDuration: 500, // milliseconds
   };
